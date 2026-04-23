e5b2a2fa7edc542525bebca513f8b92b428a6c2b
Md. Nazmul Hasan Anik <nazzmul.anik@gmail.com>
2026-04-23 11:34:06 +0600

    Crate API-Gateway service. And doing api routing for entire service api routing.

---
+++ API-GATEWAY/src/main/java/com/apiGateway/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.auth.exception;
+
+import com.auth.exception.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex){
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), ex.getStatus());
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handleBadRequestException(BadRequestException ex){
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), ex.getStatus());
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @ExceptionHandler(MissingParameterException.class)
+    public ResponseEntity<?> handleMissingParameterException(MissingParameterException ex){
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), ex.getStatus());
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+}
---
+++ API-GATEWAY/src/main/java/com/apiGateway/filter/JwtUtil.java
@@ -0,0 +1,4 @@
+package com.apiGateway.filter;
+
+public class JwtUtil {
+}
